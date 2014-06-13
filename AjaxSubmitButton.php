208f1640ecd52fa3114ad00a568f68adb47fd3a3
oleg <demogorgorn@gmail.com>
2014-06-13 22:51:07 +0300

    first commit

---
+++ AjaxSubmitButton.php
@@ -0,0 +1,104 @@
+<?php
+namespace demogorgorn\ajax;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\helpers\Json;
+
+/**
+ * AjaxSubmitButton renders an ajax button which is very similar to ajaxSubmitButton from Yii1.
+ *
+ * Example:
+ *
+ * ```php
+ * <?= Html::beginForm(); ?>
+ * <?= Select2::widget([
+ *       'name' => 'country_code',
+ *       'data' => Country::getAllCountries(),
+ *       'options' => [
+ *           'id' => 'country_select',
+ *           'multiple' => false, 
+ *           'placeholder' => 'Select country...',
+ *           'class' => 'uk-width-medium-7-10'
+ *       ]
+ *   ]);?>
+ *
+ * <?php AjaxSubmitButton::begin([
+ *       'label'=>'Проверить',
+ *       'ajaxOptions'=>
+ *           [
+ *               'type'=>'POST',
+ *               'url'=>'country/getinfo',
+ *               'cache' => false,
+ *               'success' => new \yii\web\JsExpression('function(html){
+ *                   $("#output").html(html);
+ *               }'),
+ *           ],
+ *           'options' => ['type' => 'submit'],
+ *       ]);
+ *  AjaxSubmitButton::end();?>
+ *
+ * <?= Html::endForm(); ?>
+ * ```
+ *
+ * @author Oleg Martemjanov <demogorgorn@gmail.com>
+ */
+
+class AjaxSubmitButton extends Widget
+{
+    public $ajaxOptions = [];
+
+    /**
+	 * @var array the HTML attributes for the widget container tag.
+	 */
+	public $options = [];
+
+    /**
+	 * @var string the tag to use to render the button
+	 */
+	public $tagName = 'button';
+	/**
+	 * @var string the button label
+	 */
+	public $label = 'Button';
+	/**
+	 * @var boolean whether the label should be HTML-encoded.
+	 */
+	public $encodeLabel = true;
+
+	/**
+	 * Initializes the widget.
+	 */
+	public function init()
+	{
+		parent::init();
+		if (!isset($this->options['id'])) {
+			$this->options['id'] = $this->getId();
+		}
+	}
+
+    public function run()
+    {
+    	parent::run();
+
+    	echo Html::tag($this->tagName, $this->encodeLabel ? Html::encode($this->label) : $this->label, $this->options);
+        
+        if (!empty($this->ajaxOptions))
+            $this->registerAjaxScript();
+    }
+
+    protected function registerAjaxScript()
+    {
+        $view = $this->getView();
+
+        if(!isset($this->ajaxOptions['data']) && isset($this->ajaxOptions['type']))
+            $this->ajaxOptions['data'] = new \yii\web\JsExpression('$(this).parents("form").serialize()');
+
+        $this->ajaxOptions= Json::encode($this->ajaxOptions);
+        $view->registerJs("$( '#".$this->options['id']."' ).click(function() {
+                $.ajax(". $this->ajaxOptions ."); 
+                return false;
+            });");
+    }
+
+} 
---
+++ README.md
@@ -0,0 +1,78 @@
+AjaxSubmitButton for Yii 2
+=====================================
+
+This is the AjaxSubmitButton widget that renders an ajax button which is very similar to ajaxSubmitButton from Yii1 for Yii 2. 
+
+Example of usage of the widget with form and other custom widget (in this case Select2).
+
+The view:
+```php
+use demogorgorn\ajax\AjaxSubmitButton;
+
+<?php echo Html::beginForm('', 'post', ['class'=>'uk-width-medium-1-1 uk-form uk-form-horizontal']); ?>
+      
+<?= Select2::widget([
+    'name' => 'country_code',
+    'data' => Country::getAllCountries(),
+    'options' => [
+        'id' => 'country_select',
+        'multiple' => false, 
+        'placeholder' => 'Choose...',
+        'class' => 'uk-width-medium-7-10']
+     ]);
+?>
+       
+<?php AjaxSubmitButton::begin([
+    'label' => 'Check',
+    'ajaxOptions' => [
+        'type'=>'POST',
+        'url'=>'country/getinfo',
+        /*'cache' => false,*/
+        'success' => new \yii\web\JsExpression('function(html){
+            $("#output").html(html);
+            }'),
+    ],
+    'options' => ['class' => 'customclass', 'type' => 'submit'],
+    ]);
+    AjaxSubmitButton::end();
+?>
+            
+<?php echo Html::endForm(); ?>
+
+<div id="output"></div>
+```
+
+Please note: that #output is a div element which will be updated.
+
+In controller:
+```php
+public function actionGetinfo()
+{
+    if(!isset($_POST['country_code']) || empty($_POST['country_code']))
+        return;
+
+    $code = $_POST['country_code'];
+
+    return $this->renderAjax('resultwidget', ['code' => $code]);
+}
+```
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require demogorgorn/yii2-ajaxSubmitButton "*"
+```
+
+or add
+
+```
+"demogorgorn/yii2-ajaxSubmitButton": "*"
+```
+
+to the require section of your `composer.json` file and run `composer update`.
+
