61f7be1287d4a5af0f52186dfa6f0e14201844fb
Susann Sgorzaly <sgorzaly@portrino.com>
2018-01-29 13:19:58 +0100

    added button icon

--- AjaxSubmitButton.php
+++ AjaxSubmitButton.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace demogorgorn\ajax;
 
 use yii\base\Widget;
@@ -7,7 +8,8 @@ use yii\helpers\Json;
 use yii\web\JsExpression;
 
 /**
- * AjaxSubmitButton renders an ajax button which is very similar to ajaxSubmitButton from Yii1.
+ * AjaxSubmitButton renders an ajax button which is very similar to
+ * ajaxSubmitButton from Yii1.
  *
  * Example:
  *
@@ -18,7 +20,7 @@ use yii\web\JsExpression;
  *       'data' => Country::getAllCountries(),
  *       'options' => [
  *           'id' => 'country_select',
- *           'multiple' => false, 
+ *           'multiple' => false,
  *           'placeholder' => 'Select country...',
  *           'class' => 'uk-width-medium-7-10'
  *       ]
@@ -44,109 +46,120 @@ use yii\web\JsExpression;
  *
  * @author Oleg Martemjanov <demogorgorn@gmail.com>
  */
-
 class AjaxSubmitButton extends Widget
 {
+
+    /**
+     * Icon positions
+     */
+    protected const ICON_POSITION_LEFT = 'left';
+
+    protected const  ICON_POSITION_RIGHT = 'right';
+
     public $ajaxOptions = [];
 
     /**
-	 * @var array the HTML attributes for the widget container tag.
-	 */
-	public $options = [];
+     * @var array the HTML attributes for the widget container tag.
+     */
+    public $options = [];
 
     /**
-	 * @var string the tag to use to render the button
-	 */
-	public $tagName = 'button';
-	/**
-	 * @var string the button label
-	 */
-	public $label = 'Button';
-	/**
-	 * @var boolean whether the label should be HTML-encoded.
-	 */
-	public $encodeLabel = true;
+     * @var string the tag to use to render the button
+     */
+    public $tagName = 'button';
+
+    /**
+     * @var string the button label
+     */
+    public $label = 'Button';
+
+    /**
+     * @var string The button icon.
+     */
+    public $icon;
+
+    /**
+     * @var string Icon position.
+     * Valid values are 'left', 'right'.
+     */
+    public $iconPosition = self::ICON_POSITION_LEFT;
+
+    /**
+     * @var boolean whether the label should be HTML-encoded.
+     */
+    public $encodeLabel = true;
+
     /**
      * @var string js object name.
      *      it is unused when useWithActiveForm is enabled
      */
     public $clickedButtonVarName = '_clickedButton';
+
     /**
      * @var boolean whether the button should not be used with ActiveForm.
-     *      string the id of ActiveForm to use the button with 
+     *      string the id of ActiveForm to use the button with
      */
     public $useWithActiveForm = false;
 
 
-
-	/**
-	 * Initializes the widget.
-	 */
-	public function init()
-	{
-		parent::init();
-
-		if (!isset($this->options['id'])) {
-			$this->options['id'] = $this->getId();
-		}
-	}
-
-    public function run()
+    /**
+     * Initializes the widget.
+     */
+    public function init()
     {
-    	parent::run();
+        parent::init();
 
-    	echo Html::tag($this->tagName, $this->encodeLabel ? Html::encode($this->label) : $this->label, $this->options);
-        
-        if (!empty($this->ajaxOptions)) {
-            
-            if ($this->useWithActiveForm !== false)
-                $this->registerAjaxFormScript();
-            else
-                $this->registerAjaxScript();
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
         }
     }
 
-    protected function registerAjaxScript()
+    public function run()
     {
-        $view = $this->getView();
+        parent::run();
 
-        if(!isset($this->ajaxOptions['type'])) {
-            $this->ajaxOptions['type'] = new JsExpression('$(this).parents("form").attr("method")');
+        $label = $this->encodeLabel ? Html::encode($this->label) : $this->label;
+
+        if ($this->icon !== null) {
+            $icon = Html::tag('i', '', ['class' => $this->icon]);
+            $label = strcasecmp($this->iconPosition,
+              self::ICON_POSITION_LEFT) === 0 ? sprintf('%s %s', $icon,
+              $label) : sprintf('%s %s', $label, $icon);
         }
 
-        if(!isset($this->ajaxOptions['url'])) {
-            $this->ajaxOptions['url'] = new JsExpression('$(this).parents("form").attr("action")');
+        echo Html::tag($this->tagName,
+          $label,
+          $this->options);
+
+        if (!empty($this->ajaxOptions)) {
+
+            if ($this->useWithActiveForm !== false) {
+                $this->registerAjaxFormScript();
+            } else {
+                $this->registerAjaxScript();
+            }
         }
-
-        if(!isset($this->ajaxOptions['data']) && isset($this->ajaxOptions['type']))
-            $this->ajaxOptions['data'] = new JsExpression('$(this).parents("form").serialize()');
-
-        $this->ajaxOptions= Json::encode($this->ajaxOptions);
-        $view->registerJs("$('#".$this->options['id']."').unbind('click').click(function() {
-                " . (null !== $this->clickedButtonVarName ? "var {$this->clickedButtonVarName} = this;" : "") . "
-                $.ajax(" . $this->ajaxOptions . ");
-                return false;
-            });");
     }
 
     protected function registerAjaxFormScript()
     {
         $view = $this->getView();
 
-        if(!isset($this->ajaxOptions['type'])) {
+        if (!isset($this->ajaxOptions['type'])) {
             $this->ajaxOptions['type'] = new JsExpression('$(this).attr("method")');
         }
 
-        if(!isset($this->ajaxOptions['url'])) {
+        if (!isset($this->ajaxOptions['url'])) {
             $this->ajaxOptions['url'] = new JsExpression('$(this).attr("action")');
         }
 
-        if(!isset($this->ajaxOptions['data']) && isset($this->ajaxOptions['type']))
+        if (!isset($this->ajaxOptions['data']) && isset($this->ajaxOptions['type'])) {
             $this->ajaxOptions['data'] = new JsExpression('$(this).serialize()');
+        }
 
-        $this->ajaxOptions= Json::encode($this->ajaxOptions);
+        $this->ajaxOptions = Json::encode($this->ajaxOptions);
 
-$js = <<<SEL
+        $js = <<<SEL
         $(document).unbind('beforeSubmit.{$this->useWithActiveForm}').on('beforeSubmit.{$this->useWithActiveForm}', "#{$this->useWithActiveForm}", function () {
             if ($(this).find('.has-error').length < 1) {
                 $.ajax({$this->ajaxOptions});
@@ -158,7 +171,30 @@ SEL;
         $view->registerJs($js);
 
 
+    }
 
+    protected function registerAjaxScript()
+    {
+        $view = $this->getView();
+
+        if (!isset($this->ajaxOptions['type'])) {
+            $this->ajaxOptions['type'] = new JsExpression('$(this).parents("form").attr("method")');
+        }
+
+        if (!isset($this->ajaxOptions['url'])) {
+            $this->ajaxOptions['url'] = new JsExpression('$(this).parents("form").attr("action")');
+        }
+
+        if (!isset($this->ajaxOptions['data']) && isset($this->ajaxOptions['type'])) {
+            $this->ajaxOptions['data'] = new JsExpression('$(this).parents("form").serialize()');
+        }
+
+        $this->ajaxOptions = Json::encode($this->ajaxOptions);
+        $view->registerJs("$('#" . $this->options['id'] . "').unbind('click').click(function() {
+                " . (null !== $this->clickedButtonVarName ? "var {$this->clickedButtonVarName} = this;" : "") . "
+                $.ajax(" . $this->ajaxOptions . ");
+                return false;
+            });");
     }
 
 } 
